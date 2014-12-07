f93f87268ba7f14682c415e1ae5ee2b3c3b11de7
renatodex <renatodex@gmail.com>
2014-12-07 15:14:39 -0200

    Added Death system to Player

--- Assets/Scripts/Enemy/EnemyController.cs
+++ Assets/Scripts/Enemy/EnemyController.cs
@@ -4,6 +4,7 @@ using System.Collections;
 public class EnemyController : MonoBehaviour {
 	public float enemyVelocity;
 	private GameObject target;
+	public float killDistance;
 
 	// Use this for initialization
 	public void Start () {
@@ -12,11 +13,27 @@ public class EnemyController : MonoBehaviour {
 	
 	// Update is called once per frame
 	public void Update () {
+		Vector3 facing = new Vector3(0f,0f,0f);
+		Vector3 attackLine;
 		float direction;
+
 		if(this.transform.position.x > targetPositionX()) {
 			direction = -1;
+			facing = Vector3.left;
 		} else {
 			direction = 1;
+			facing = Vector3.right;
+		}
+
+		attackLine = this.transform.position+facing*this.killDistance;
+
+		Debug.DrawLine(this.transform.position, attackLine, Color.yellow, 0.2f);
+
+		RaycastHit2D hit = Physics2D.Linecast(this.transform.position, attackLine, 1 << LayerMask.NameToLayer("Player"));
+		if(hit) {
+			if(hit.collider.tag == "Player") {
+				Destroy(hit.collider.gameObject);
+			}
 		}
 
 		rigidbody2D.velocity = new Vector2 (direction*(this.enemyVelocity), rigidbody2D.velocity.y);
