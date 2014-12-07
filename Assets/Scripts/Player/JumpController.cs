7c1338e4ff9151839572ee5037c797bd2b426f5d
renatodex <renatodex@gmail.com>
2014-12-07 01:04:40 -0200

    Added Jump System

---
+++ Assets/Scripts/Player/JumpController.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class JumpController : MonoBehaviour {
+
+	public KeyCode jumpKey;
+	public Vector2 jumpForce;
+
+	// Use this for initialization
+	public void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	public void FixedUpdate () {
+		if(Input.GetKeyDown(this.jumpKey) && this.isGrounded()){ 
+			this.rigidbody2D.AddForce(this.jumpForce);
+		}
+	}
+
+	private bool isGrounded() {
+		Debug.DrawRay(transform.position, Vector3.down*2f, Color.blue, 1);
+		if (Physics2D.Raycast(transform.position, Vector3.down, 2f, 1 << LayerMask.NameToLayer("Ground"))) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+}
